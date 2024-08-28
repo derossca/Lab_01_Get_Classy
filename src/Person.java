95c821cab4d4de5e234dc73ab29348232a4afbea
derossca <derossca@mail.uc.edu>
2024-08-28 13:13:35 -0400

    made methods, setters, and getters for Persona java class

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,81 @@
+import java.util.Calendar;
+
 public class Person {
+    private String IDNum;
+    private String firstName;
+    private String lastName;
+    private String title;
+    private int YOB;
+
+    public Person(String IDNum, String firstName, String lastName, String title, int YOB) {
+        this.IDNum = IDNum;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.title = title;
+        this.YOB = YOB;
+    }
+
+    public String fullName() {
+        return firstName + " " + lastName;
+    }
+
+    public String formalName() {
+        return title + " " + fullName();
+    }
+
+    public String getAge(){
+        Calendar cal = Calendar.getInstance();
+        int currentYear = cal.get(Calendar.YEAR);
+        int age = currentYear - YOB;
+        return String.valueOf(age);
+    }
+
+    public String getAge(int currentYear){
+        int age = currentYear - YOB;
+        return String.valueOf(age);
+    }
+
+    public String toCSVDataRecord() {
+        return this.IDNum + "," + this.firstName + "," + this.lastName + "," + this.title + "," + this.YOB;
+    }
+
+    public String getIDNum() {
+        return IDNum;
+    }
+
+    public void setIDNum(String IDNum) {
+        this.IDNum = IDNum;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYOB() {
+        return YOB;
+    }
+
+    public void setYOB(int YOB) {
+        this.YOB = YOB;
+    }
 }
