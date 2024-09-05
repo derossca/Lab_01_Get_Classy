ac1f9d25ae9321ae0f3478d76eb8d89c480ebf13
derossca <derossca@mail.uc.edu>
2024-09-05 08:25:04 -0400

    added comments

--- src/Person.java
+++ src/Person.java
@@ -15,14 +15,29 @@ public class Person {
         this.YOB = YOB;
     }
 
+    /**
+     * Get a String which contains the first and last name of the Person
+     * @return a String of the Person's first and last name
+     */
     public String fullName() {
         return firstName + " " + lastName;
     }
 
+    /**
+     * Get a String which contains the title and last name of the Person
+     * @return a String of the Person's title and last name
+     */
     public String formalName() {
         return title + " " + fullName();
     }
 
+    /**
+     * Get a String which contains the age of the person
+     * make calendar instance
+     * get current year
+     * age is the current year minus their year of birth
+     * @return a String of the person's age
+     */
     public String getAge(){
         Calendar cal = Calendar.getInstance();
         int currentYear = cal.get(Calendar.YEAR);
@@ -30,11 +45,20 @@ public class Person {
         return String.valueOf(age);
     }
 
+    /**
+     * Get a String which determines the age of the person
+     * @param  int of current year
+     * @return a String of the Person's age
+     */
     public String getAge(int currentYear){
         int age = currentYear - YOB;
         return String.valueOf(age);
     }
 
+    /**
+     * Get a String which contains the entire CSVDataRecord
+     * @return a String of the Person's ID, first name, last name, title, and year of birth
+     */
     public String toCSVDataRecord() {
         return this.IDNum + "," + this.firstName + "," + this.lastName + "," + this.title + "," + this.YOB;
     }
