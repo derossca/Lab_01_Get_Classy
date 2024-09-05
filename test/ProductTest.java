bf32ca813c0e46aeb266bf00ffdaa21cf38de6a2
derossca <derossca@mail.uc.edu>
2024-09-05 14:18:25 -0400

    set up junit test for Product

---
+++ test/ProductTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void setID() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void setDescript() {
+    }
+
+    @Test
+    void setCost() {
+    }
+
+    @Test
+    void toCSVDataRecord() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
