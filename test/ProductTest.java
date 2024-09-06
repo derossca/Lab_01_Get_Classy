9c7b2d896845fd6d81f83bfbdc801db15da2238e
derossca <derossca@mail.uc.edu>
2024-09-06 09:51:07 -0400

    made junit tests for producttest

--- test/ProductTest.java
+++ test/ProductTest.java
@@ -5,28 +5,37 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTest {
 
+    private Product product;
+
     @BeforeEach
     void setUp() {
+        product = new Product("000001", "Pipeweed","Long Bottom Leaf",600.0);
     }
 
     @Test
     void setID() {
+        assertEquals("000001", product.getID());
     }
 
     @Test
     void setName() {
+        assertEquals("Pipeweed", product.getName());
     }
 
     @Test
     void setDescript() {
+        assertEquals("Long Bottom Leaf", product.getDescript());
     }
 
     @Test
     void setCost() {
+        assertEquals(600.0, product.getCost());
     }
 
     @Test
     void toCSVDataRecord() {
+        assertEquals("000001,Pipeweed,Long Bottom Leaf,600.0", product.toCSVDataRecord());
+
     }
 
     @Test
